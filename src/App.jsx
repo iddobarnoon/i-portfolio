d19736eefe1eb8e7c016ce7f9cff6592073cf150
Iddo Barnoon <iddobarnoon@gmail.com>
2025-09-02 15:16:20 -0400

    Navbar implemented, started mobile hamburger

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css';
 import { LoadingScreen } from './components/LoadingScreen';
+import { NavBar } from './components/Navbar';
 import "./index.css";
 import { useState } from "react";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return(
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      <div className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        
+      </div>
     </>
     );
 }
