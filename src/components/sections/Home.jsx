075259eaf2fe9d96b85e1308e90c89dbfeba5370
Iddo Barnoon <iddobarnoon@gmail.com>
2025-09-02 23:00:18 -0400

    buttons on home

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -6,6 +6,10 @@ export const Home = () => {
                 Hi, I'm Iddo.
             </h1>
             <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST </p>
+            <div className="flex justify-center space-x-4">
+                <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,247,0.4)]">View Projects</a>
+                <a href="#projects" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,247,0.2)] hover:bg-blue-500/10">Contact Me</a>
+            </div>
         </div>
     </section>);
 }
