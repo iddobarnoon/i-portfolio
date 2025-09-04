1e7a5ac702d9c202697c964a0000713f03c45da5
Iddo Barnoon <iddobarnoon@gmail.com>
2025-09-04 15:13:10 -0400

    remove uneeded experience and education

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -59,24 +59,7 @@ export const About = () => {
                     </div>
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-                        <h3 className="text-xl font-bold mb-4">Education</h3>
-                        <ul list-disc className="list-disc list-inside text-gray-300 space-y-2">
-                            <li>
-                                <strong> B.S in Computer Science, Minor in Mathematics</strong> - Yeshiva University (2024-Present)
-                            </li>
-                            <li>Relevant Coursework: Data Structures, Introduction to Algorithms, Computer Architecture and Assembly Language Programming, Calculus II, Linear Algebra</li>
-                        </ul>
-                    </div>
-                    <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-                        <h3 className="text-xl font-bold mb-4">Work</h3>
-                        <div className="space-y-4 text-gray-300">
-                            <h4 className="font-semibold"> Machine Learning Engineering Intern @ Lendbuzz (Jun 2025-Aug 2025)</h4>
-                            <p>Blah Blah</p>
-                        </div>
-                    </div>
-                </div>
+                
 
                 <div className="flex justify-center items-center w-full mt-8">
                     <div className="w-[600px] mx-auto">
