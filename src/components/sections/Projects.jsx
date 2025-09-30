a40844aea31f62abab872d402a96d7e8792279e8
Iddo Barnoon <iddobarnoon@gmail.com>
2025-09-29 21:53:45 -0400

    add ml project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -25,6 +25,24 @@ export const Projects = () => {
                         </div>
                     </div>
 
+                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                        <h3 className="text-xl font-bold mb-2">Premier League Win Predictor </h3>
+                        <p className="text-gray-400 mb-4">Using a random forest machine learning model, predicts future EPL winners by analyzing historical football data</p>
+                        <div className="flex flex-wrap gap-2 mb-4">
+                            {["Jupyter Notebook", "Scikit-learn", "Pandas", "NumPy"].map((tech, key) => (
+                                <span className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                                        {tech}
+                                    </span>
+                            ))}
+                        </div>
+                        <div className="flex justify-between items-center">
+                            <a href="https://github.com/iddobarnoon/PremierLeagueWinnersML" className="text-blue-400 hover:text-blue-300 transition-colors my-4">
+                                View Project &#8594;
+                            </a>
+                        </div>
+                    </div>
+                    
+
                 </div>
             </div>
             </RevealOnScroll>
